59fa9846ee8836a477803085afaca0914d2329ed
I524960 <hogan.he@sap.com>
2021-11-04 18:17:36 +0800

    theInitFramework:ui5=>manifest.json=>Compoent.js=>rootview

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,20 +1,8 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
-	"use strict";
-
-	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-
-		onShowHello : function () {
-			// read msg from i18n model
-			var oBundle = this.getView().getModel("i18n").getResourceBundle();
-			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-			// show message
-			MessageToast.show(sMsg);
-		}
-	});
-
-});
+sap.ui.define(['sap/ui/core/mvc/Controller'], function (Controller) {
+    'use strict';
+  
+    return Controller.extend('UI5PROJECT.controller.App', {
+      onInit: function () {}
+    });
+  });
+  
